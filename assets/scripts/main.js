e9d21ec330d42ac43a5eb3df488be8ec3a586757
Jonathan Zong <jonathan.zong@gmail.com>
2019-03-04 00:35:08 -0500

    boom

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,16 +4,36 @@ $(document).ready(function() {
     current = 0,
     timeout = 5000;
 
-  function changeSlide() {
-    element.removeClass('active');
+  if (element) {
+    function changeSlide() {
+      element.removeClass('active');
 
-    element.eq(current++).addClass('active');
-    if(current === length){
-        current = 0;
+      element.eq(current++).addClass('active');
+      if(current === length){
+          current = 0;
+      }
+
+      setTimeout(changeSlide, timeout);
     }
 
-    setTimeout(changeSlide, timeout);
+    changeSlide();
   }
 
-  changeSlide();
+  $('.nav .button').click(function() {
+    var menuIsOpening = $('.nav .burger').hasClass('active');
+    if (menuIsOpening) {
+      $('.nav .burger').removeClass('active');
+      $('.nav .close').addClass('active');
+
+      $('.nav .nav-menu').addClass('active');
+    }
+    else {
+      $('.nav .close').removeClass('active');
+      $('.nav .burger').addClass('active');
+
+      $('.nav .nav-menu').removeClass('active');
+    }
+  });
+
+  $('.nav a.active').click(function() {return false});
 });
