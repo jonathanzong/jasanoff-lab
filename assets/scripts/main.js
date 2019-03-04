55471a5a178fae7660a3e92758e87b6f3a0929f6
Jonathan Zong <jonathan.zong@gmail.com>
2019-03-04 00:04:35 -0500

    all pages

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,19 @@
 $(document).ready(function() {
+  var element = $('.index img'),
+    length = element.length,
+    current = 0,
+    timeout = 5000;
+
+  function changeSlide() {
+    element.removeClass('active');
+
+    element.eq(current++).addClass('active');
+    if(current === length){
+        current = 0;
+    }
+
+    setTimeout(changeSlide, timeout);
+  }
+
+  changeSlide();
 });
